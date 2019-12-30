5404f915fdcf040cfac6e71308e6ab503f1577e5
Brandon Sturgeon <sturgeonb4@gmail.com>
2019-12-30 01:09:45 -0500

    Parse failure codes correctly, too

--- lib/systemd_status_parser.py
+++ lib/systemd_status_parser.py
@@ -8,19 +8,33 @@ class SystemdStatusParser():
         output = output.split("\n")
 
         # Active: active (running) since Wed 2019-12-18 19:58:50 EST; 1 day 8h ago
+        # Active: failed (Result: signal) since Mon 2019-12-30 00:06:14 EST; 55min ago
         activity_line = output[2]
 
         # ['Active:', 'active', '(running)', 'since', 'Wed', '2019-12-18', '19:58:50', 'EST;', '1', 'day', '8h', 'ago']
+        # ['Active:', 'failed', '(Result:', 'signal)', 'since', 'Mon', '2019-12-30', '00:06:14', 'EST;', '55min', 'ago']
         activity_line_spl = activity_line.split()
 
         # 'active'
+        # 'failed'
         activity_status = activity_line_spl[1]
+
+        # 3
+        # 4
+        since = activity_line_spl.index("since")
+
+        # ['active', '(running)']
+        # ['failed', '(Result:', 'signal)']
+        activity_line = activity_line_spl[1:since]
+
+        # 'active (running)'
+        # 'failed (Result: signal)'
+        activity_line = " ".join(activity_line)
 
         status = {
             "status": activity_status,
-            "activity_line": activity_line.split(": ")[1]
+            "activity_line": activity_line
         }
-
 
         return status
 
